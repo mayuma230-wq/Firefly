e9dc94067c9d068b38c826ae95e4e5d10266040d
mayuma230-wq <mayuma230@gmail.com>
2026-09-16 16:03:25 +0800

    更新 profileConfig.ts

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -8,7 +8,7 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/"开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar: "/assets/images/avatar.svg",
+	avatar: "https://cloudflare-imgbed.mayuma230.workers.dev/file/1789538393107_mmexport1789533258263.jpg",
 
 	// 下班时间头像（为空则始终使用上方 avatar）
 	avatarOffWork: "assets/images/xiaban.gif",
